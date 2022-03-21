f03c8a3432ac94209a34ed34e55c5370c30650b2
rushilkoundal <rushilkoundal1611@gmail.com>
2022-03-21 19:02:29 +0530

    Update

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -6,6 +6,20 @@ function Sidebar({ showSidebar, sidebar }) {
             <span  className="chevron" >
                 <ion-icon name="chevron-back-outline" onClick={() => { showSidebar(false) }}></ion-icon>
             </span>
+            <div className="center-column-right">
+            <ul>
+                <li>
+                    <a to='/' className='nav-link'>Your Acoount</a>
+                    <a to='/' className='nav-link'>Create Blog</a>
+                    <a to='/' className='nav-link'>Home</a>
+                    <a to='/' className='nav-link'>News</a>
+                    <a to='/' className='nav-link'>Business</a>
+                    <a to='/' className='nav-link'>Sociology</a>
+                    <a to='/' className='nav-link'>Tech</a>
+                    <a to='/' className='nav-link'>Economics</a>
+                </li>
+            </ul>
+        </div>
         </div>
     )
 }
